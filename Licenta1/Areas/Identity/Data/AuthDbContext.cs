2cd66ca644309bab0531ecca430768227eedcc05
Alex Ceuca <alex_ceuca@yahoo.com>
2024-10-01 20:58:21 +0300

    Add project files.

---
+++ Licenta1/Areas/Identity/Data/AuthDbContext.cs
@@ -0,0 +1,22 @@
+﻿using Licenta1.Areas.Identity.Data;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace Licenta1.Data;
+
+public class AuthDbContext : IdentityDbContext<AplicationUser>
+{
+    public AuthDbContext(DbContextOptions<AuthDbContext> options)
+        : base(options)
+    {
+    }
+
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+        base.OnModelCreating(builder);
+        // Customize the ASP.NET Identity model and override the defaults if needed.
+        // For example, you can rename the ASP.NET Identity table names and more.
+        // Add your customizations after calling base.OnModelCreating(builder);
+    }
+}
