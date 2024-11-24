7b91079a33eedacbbf64f9de2440451ca1dacaf6
Alex Ceuca <alex_ceuca@yahoo.com>
2024-11-24 12:54:02 +0200

    pop-ups + corect dates for booking, cant let the user choose more preiods

---
+++ Licenta1/Migrations/20241124091054_cartlogic.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DolphinsSunsetResort.Migrations
+{
+    /// <inheritdoc />
+    public partial class cartlogic : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "CheckInDate",
+                table: "Cart");
+
+            migrationBuilder.DropColumn(
+                name: "CheckOutDate",
+                table: "Cart");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "CheckInDate",
+                table: "Cart",
+                type: "datetime2",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "CheckOutDate",
+                table: "Cart",
+                type: "datetime2",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+        }
+    }
+}
---
+++ Licenta1/Views/ViewsModel/RoomFilterViewModel.cs
@@ -0,0 +1,11 @@
+﻿using DolphinsSunsetResort.Models;
+
+namespace DolphinsSunsetResort.Views.ViewsModel
+{
+	public class RoomFilterViewModel
+	{
+		public List<Room> Rooms { get; set; }
+		public DateTime? CheckInDate { get; set; }
+		public DateTime? CheckOutDate { get; set;}
+	}
+}
