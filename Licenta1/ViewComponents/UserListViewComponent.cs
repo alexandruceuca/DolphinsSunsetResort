3df05ebebc7c21aac2b1bd3d08cbd64075066d9c
Alex Ceuca <alex_ceuca@yahoo.com>
2024-12-08 00:36:03 +0200

    userlist filter by mail

---
+++ Licenta1/ViewComponents/UserListViewComponent.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace DolphinsSunsetResort.ViewComponents
+{
+    using Microsoft.AspNetCore.Mvc;
+    using Microsoft.AspNetCore.Identity;
+    using System.Linq;
+    using System.Threading.Tasks;
+    using DolphinsSunsetResort.Views.ViewsModel;
+    using DolphinsSunsetResort.Areas.Identity.Data;
+    using Microsoft.EntityFrameworkCore;
+    using Microsoft.AspNetCore.Authorization;
+
+    [Authorize(Roles = "Admin")]
+    public class UserListViewComponent : ViewComponent
+    {
+        private readonly UserManager<AplicationUser> _userManager;
+
+        public UserListViewComponent(UserManager<AplicationUser> userManager)
+        {
+            _userManager = userManager;
+        }
+        [Authorize(Roles = "Admin")]
+        public async Task<IViewComponentResult> InvokeAsync(string emailFilter )
+        {
+            var users = await _userManager.Users
+                .Where(u => string.IsNullOrEmpty(emailFilter) || u.Email.Contains(emailFilter))
+                .Select(u => new UserWithRolesViewModel
+                {
+                    Id = u.Id,
+                    FirstName = u.FirstName,
+                    LastName = u.LastName,
+                    Email = u.Email,
+                    Roles = _userManager.GetRolesAsync(u).Result.ToList()
+                })
+                .ToListAsync();
+
+            return View("/Views/Shared/Components/Roles/Admin/UserList.cshtml",users);
+        }
+    }
+
+}
---
+++ Licenta1/wwwroot/js/filterUserList.js
@@ -0,0 +1,22 @@
+﻿
+$(document).ready(function () {
+    $('#emailFilter').on('keypress', function (e) {
+        // Check if the Enter key is pressed
+        if (e.key === "Enter") {
+            const emailFilter = $(this).val();
+            const url = $('#emailFilter').data('url');
+
+            $.ajax({
+                url: url,
+                type: 'GET',
+                data: { emailFilter: emailFilter },
+                success: function (result) {
+                    $('#accountsTable').html(result);
+                },
+                error: function () {
+                    Swal.fire('Error', 'Unable to fetch accounts. Please try again.', 'error');
+                }
+            });
+        }
+    });
+});
