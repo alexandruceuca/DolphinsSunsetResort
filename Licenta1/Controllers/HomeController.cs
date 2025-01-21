870c0c4f93065b728ae5c7e6202d242fb8e08c66
Alex Ceuca <alex_ceuca@yahoo.com>
2025-01-21 21:29:13 +0200

    statistics on home page

--- Licenta1/Controllers/HomeController.cs
+++ Licenta1/Controllers/HomeController.cs
@@ -8,40 +8,63 @@ using System.Diagnostics;
 
 namespace DolphinsSunsetResort.Controllers
 {
-    public class HomeController : Controller
-    {
-        private readonly ILogger<HomeController> _logger;
+	public class HomeController : Controller
+	{
+		private readonly ILogger<HomeController> _logger;
 		private readonly AuthDbContext _context;
 
 		public HomeController(ILogger<HomeController> logger, AuthDbContext context)
-        {
-            _logger = logger;
+		{
+			_logger = logger;
 			_context = context;
 		}
 
-        public IActionResult Index()
-        {
-			var bookingCount=_context.Bookings.Where(b=>b.CheckInDate.Year==(DateTime.Now.Year-1)).Count();
-            ViewBag.BookingsCount = bookingCount;
-            return View();
-        }
+		public async Task<IActionResult> Index()
+		{
+			var bookingCount = _context.Bookings.Where(b => b.CheckInDate.Year == (DateTime.Now.Year - 1)).Count();
+
+
+			var totalRecommendations = await _context.Bookings.CountAsync(b => b.Status == BookingStatus.CheckOut && b.RecommendationId != (int)Recommendation.NoComment);
+			var yesRecommendations = await _context.Bookings
+				.CountAsync(b => b.RecommendationId == (int)Recommendation.Yes);
+			var percentageYes = totalRecommendations > 0
+							? (yesRecommendations / (double)totalRecommendations) * 100
+							: 0;
+
+			var rating = _context.Bookings.Where(b => b.Status == BookingStatus.CheckOut && b.Rating!=0).ToList();
+
+		
+
+			var ratingFinal = 0;
+
+			foreach (var booking in rating)
+			{
+				ratingFinal = booking.Rating + ratingFinal;
+			}
+			ratingFinal = ratingFinal / rating.Count();
+
+			ViewBag.BookingsCount = bookingCount;
+			ViewBag.RatingFinal = ratingFinal;
+			ViewBag.Recommendations = percentageYes;
+			return View();
+		}
 
 
 
 
 		public IActionResult Facilities()
-        {
-            return View();
-        }
-        public IActionResult Contact()
-        {
-            return View();
-        }
+		{
+			return View();
+		}
+		public IActionResult Contact()
+		{
+			return View();
+		}
 
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
-        }
-    }
+		[ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+		public IActionResult Error()
+		{
+			return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+		}
+	}
 }
